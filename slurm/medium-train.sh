4bd1f7c9b5b9b6d9d5bd19d9df1a1dc5b39b3c67
Dhruva Sundararajan <dhruva.sundararajan@gmail.com>
2025-05-05 13:49:20 -0400

    runs

---
+++ slurm/medium-train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -J medium-train
+#SBATCH --account=lgo
+#SBATCH --partition=l40s_normal_q
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --time=1-00:00:00
+#SBATCH --output=medium-train.out
+#SBATCH --error=medium-train.err
+
+# Activate your environment
+source ~/.bashrc
+module load Miniconda3
+source activate gnn_xiong_env
+
+# Run your GNN training script
+python /home/dhruvasundar/all_feasible/codes/Encoding.py -d medium
+python /home/dhruvasundar/all_feasible/codes/NeuralPrediction.py -d medium
+
+# Deactivate your environment
+conda deactivate
